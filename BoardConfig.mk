123bb454dc07d903a3f98686d395089ddecc0f56
dopaemon <43093213+dopaemon@users.noreply.github.com>
2020-02-23 22:29:02 +0700

    Try Build Without Source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,3 +10,7 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 -include vendor/vsmart/V620A/BoardConfigVendor.mk
+
+TARGET_KERNEL_SOURCE := kernel/vsmart/V620A
+
+TARGET_PREBUILT_KERNEL := Image.gz-dtb
